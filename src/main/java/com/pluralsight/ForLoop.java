f986544508ff1c16ce79dbdc6215e87bc571cfb8
NickBinns <tallnickbinns14@gmail.com>
2024-04-18 13:32:47 -0400

    Completed looping exercises

---
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int z = 10; z > 0; z -= 1) {
+            System.out.println(z);
+        }
+        System.out.print("Launch!");
+    }
+}
